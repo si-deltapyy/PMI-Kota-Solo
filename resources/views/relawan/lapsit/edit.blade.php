3d9cffef29bf05db2d9e8eab9a4a87d9a2df7be4
sekargestiau <sekargestiau@gmail.com>
2024-06-27 10:33:54 +0700

    update edit assessment + lapsit

--- resources/views/relawan/lapsit/edit.blade.php
+++ resources/views/relawan/lapsit/edit.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-12 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Laporan Assessment</h4>
+                            <h4 class="card-title">Laporan Situasi</h4>
                             <!-- Update Alert-->
                             @if(session('success'))
                             <div class="alert alert-success">
